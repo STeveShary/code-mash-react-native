56a874973bf445b7ff7b89fc9a60a6af68e31550
STeve Shary <stephen.shary@kroger.com>
2017-11-30 22:07:17 -0500

    Adding speaker info

--- screens/SpeakerScreen.js
+++ screens/SpeakerScreen.js
@@ -4,39 +4,74 @@ import {
   View,
   Image,
   StyleSheet,
+  Linking,
+  ScrollView,
 } from 'react-native';
 
-const speakerInfo = { "Id": "f62ed587-5dab-4818-b0f3-398f7975961a", 
-  "FirstName": "Rob", 
-  "LastName": "Allen", 
-  "Biography": "Rob Allen is a software consultant and developer. He has been involved in software architecture and development for many years and writes code in PHP, Swift and other interesting languages. He is part of Slim Framework's leadership team and contributes to Apache OpenWhisk & other open source projects. Rob is a published author and based in the UK where he runs [Nineteen Feet Limited](http://19ft.com), focussing on API development, training and consultancy. In his spare time, Rob blogs at [akrabat.com](https://akrabat.com) and can often be seen with a camera in his hand.", 
-  "GravatarUrl": "//www.gravatar.com/avatar/2f3857ea7ae880357bc66f7314eb5355", 
-  "TwitterLink": "https://twitter.com/akrabat", 
-  "GitHubLink": "https://github.com/akrabat", 
-  "LinkedInProfile": "https://www.linkedin.com/in/akrabat", 
-  "BlogUrl": "https://akrabat.com", 
-  "SessionIds": ["7466"] 
+const speakerInfo = {
+  "Id": "f62ed587-5dab-4818-b0f3-398f7975961a",
+  "FirstName": "Christopher",
+  "LastName": "Allen Alleshouse",
+  "Biography": "Rob Allen is a software consultant and developer. He has been involved in software architecture and development for many years and writes code in PHP, Swift and other interesting languages. He is part of Slim Framework's leadership team and contributes to Apache OpenWhisk & other open source projects. Rob is a published author and based in the UK where he runs [Nineteen Feet Limited](http://19ft.com), focussing on API development, training and consultancy. In his spare time, Rob blogs at [akrabat.com](https://akrabat.com) and can often be seen with a camera in his hand.",
+  "GravatarUrl": "//www.gravatar.com/avatar/2f3857ea7ae880357bc66f7314eb5355",
+  "TwitterLink": "https://twitter.com/akrabat",
+  "GitHubLink": "https://github.com/akrabat",
+  "LinkedInProfile": "https://www.linkedin.com/in/akrabat",
+  "BlogUrl": "https://akrabat.com",
+  "SessionIds": ["7466"]
 };
 
 
 class SessionScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.speakerInfo = speakerInfo;
+    this._openBlog = this._openBlog.bind(this);
+  }
+
   static navigationOptions = {
     title: 'Speaker'
   };
 
   render() {
     return (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-       <Image
-          style={{width: 66, height: 58}}
-          source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-        />
-        <Text>{speakerInfo.FirstName} {speakerInfo.LastName}</Text>
-    { speakerInfo.BlogUrl && <Text>{speakerInfo.BlogUrl}</Text> }
-        <Text>{speakerInfo.Biography}</Text>
-        <Text onPress={this._handlePress}>Go Home!</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Image style={styles.bioImage}
+            source={{ uri: "https:" + speakerInfo.GravatarUrl }} />
+          <View style={styles.nameContainer}>
+            <Text style={styles.speakerName}>{speakerInfo.FirstName}</Text>
+            <Text style={styles.speakerName}>{speakerInfo.LastName}</Text>
+           {speakerInfo.BlogUrl && <Text style={styles.link} onPress={this._openBlog}>Blog</Text>}
+           {speakerInfo.LinkedInProfile && <Text style={styles.link} onPress={this._openLinkedIn}>LinkedIn</Text>}
+           {speakerInfo.TwitterLink && <Text style={styles.link} onPress={this._openTwitter}>Twitter</Text>}
+           {speakerInfo.GitHubLink && <Text style={styles.link} onPress={this._openGithub}>GitHub</Text>}
+          </View>
+        </View>
+        <Text style={styles.bioContainer}>{speakerInfo.Biography}</Text>
+        <View style={styles.sessionContainer}>
+          <Text style={styles.sessionTitle}>Sessions</Text>
+          <Text>Sessions to come!</Text>
+        </View>
+      </ScrollView>
     )
+  }
+
+  _openBlog = () => {
+      Linking.openURL(this.speakerInfo.BlogUrl);
+  }
+
+  _openLinkedIn = () => {
+    Linking.openURL(this.speakerInfo.LinkedInProfile)
+  }
+
+  _openTwitter = () => {
+    Linking.openURL(this.speakerInfo.TwitterLink)
+  }
+
+  _openGithub = () => {
+    Linking.openURL(this.speakerInfo.GitHubLink)
   }
 
   _handlePress = () => {
@@ -45,11 +80,49 @@ class SessionScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  sessionContainer: {
+    flex: 1,
+    padding: 20,
+  },
+  sessionTitle: {},
+  link: {
+    fontWeight: "200",
+    color: 'blue',
+    paddingTop: 10,
+    fontSize: 20,
+  },
+  speakerName: {
+    fontSize: 15,
+    fontWeight: "700",
+  },
+  headerContainer: {
+    flex: 1,
+    flexDirection: "row",
+    height: 200,
+    paddingLeft: 15,
+    paddingTop: 15,
+  },
+  nameContainer: {
+    flex: 0.4,
+    flexDirection: "column",
+    paddingLeft: 10,
+  },
+  bioImage: {
+    flex: 0.5,
+    alignSelf: 'stretch',
+    width: 200,
+    height: 200,
+    padding: 10,
+  },
+  bioContainer: {
+    flex: 1,
+    padding: 20,
+    fontSize: 15,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#1a84a7',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
   },
 });
 
